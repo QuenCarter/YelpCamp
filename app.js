ada864194b592956434bab46f2f2ea2865c334bf
Quen Carter <quen.carter@outlook.com>
2020-03-04 13:54:48 -0500

    connected to MogoDB

--- app.js
+++ app.js
@@ -1,38 +1,69 @@
-let express = require("express");
-let app = express();
-let bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
-let campgrounds = [
-    {name: "Proud Lake Campground", image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
-    {name: "Haas Lake Park RV Campground", image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
-    {name: "Pontiac Lake State Recreation Area Campground", image: "https://images.unsplash.com/photo-1568576550491-185584b2145a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
-    {name: "Detroit Greenfield Campground", image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
-];
+const uri = "mongodb+srv://mongooseTesting:SD3LAia6C@qcode-xy9yt.mongodb.net/quen_yelp?retryWrites=true&w=majority";
 
-app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log(`Database Connected`);
+    })
+    .catch(error => {
+        console.log(`You have an error: ${error}`);
+    });
+
+//schema set up
+
+const campgroundsSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundsSchema);
+    
+//     { name: "Pontiac Lake State Recreation Area Campground", image: "https://images.unsplash.com/photo-1568576550491-185584b2145a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" },
+//     { name: "Detroit Greenfield Campground", image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" },
+
+
+// need this for post endpoints. to grab parsed data from req.body
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
 
-    res.render("campgrounds", {camps:campgrounds});
+        Campground.find({}, (error, grounds) =>{
+        if (error){
+            console.log(`error: ${error}`);
+        }else{
+            res.render("campgrounds", { camps: grounds });
+        }
+    }
+    );
+
 });
 
 app.post("/campgrounds", (req, res) => {
     let campground = {
-        name:req.body.name, 
+        name: req.body.name,
         image: req.body.image
     };
-    campgrounds.push(campground);
-    res.redirect("/campgrounds");
+    Campground.create(campground, (error, newCamp) =>{
+        if (error){
+            console.log(`Could not add new camp: ${error}`);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
 });
 app.get("/campgrounds/new", (req, res) => {
     res.render("new");
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("Server started")
 })
